4a5fcf62a99f4a8633020fbf836aff64819a720f
한지훈 <uuiop0129@gmail.com>
2022-09-30 11:54:42 +0900

    ..

--- business/js/common.js
+++ business/js/common.js
@@ -1,20 +1,4 @@
-<<<<<<< HEAD
-=======
 
-var menu = document.getElementById("m_btn");
-var gnb =  document.getElementById("gnb");
-gnb.setAttribute("style","display:none");
-menu.onclick = function(){
-	if(gnb.style.display == "none"){
-		gnb.style.display ="block";
-	}else{
-		gnb.style.display = "none";
-	}
-	
-}
-
-
->>>>>>> 98fd3ee0a0ec62d6ac36f056eaaba3a257f17dc2
 /* 햄버거 버튼 변형 */
 var burger = $('#m_btn');
 burger.each(function(index){
@@ -23,7 +7,6 @@ burger.each(function(index){
     e.preventDefault();
     $(this).toggleClass('active-' + (index+1));
 	})
-<<<<<<< HEAD
 });
 
 
@@ -32,6 +15,4 @@ $(function(){
 		$("#gnb").fadeToggle();
 	});
 });
-=======
-});
->>>>>>> 98fd3ee0a0ec62d6ac36f056eaaba3a257f17dc2
+
