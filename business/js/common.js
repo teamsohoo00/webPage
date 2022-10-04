664779221d22112fbb22c5233217ff4b0748eb20
junsunny <wnstjsektzja@naver.com>
2022-10-04 15:50:53 +0900

    2022-10-04 b-1 장바구니

--- business/js/common.js
+++ business/js/common.js
@@ -8,3 +8,19 @@ menu.onclick = function(){
 		gnb.style.display = "none";
 	}
 }
+/* 햄버거 버튼 변형 */
+var burger = $('#m_btn');
+burger.each(function(index){
+	var $this = $(this);
+	$this.on('click', function(e){
+    e.preventDefault();
+    $(this).toggleClass('active-' + (index+1));
+	})
+});
+
+
+$(function(){
+	$("#m_btn").click(function(){
+		$("#gnb").fadeToggle();
+	});
+});
