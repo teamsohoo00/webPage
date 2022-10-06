327affe58c97686089e895616c3f568b2bfa9350
junsunny <wnstjsektzja@naver.com>
2022-10-06 12:27:40 +0900

    2022-10-06 동화약품 최최종

--- brand/donghwa/js/jquery.js
+++ brand/donghwa/js/jquery.js
@@ -4,6 +4,8 @@
 
 /*헤더*/
 /*product 캐러셀*/
+
+
 var n =0;
 $("#carousel a").eq(n).children("img").attr("src","./images/btn_banner_ov.png");
 
@@ -17,7 +19,7 @@ function count(){
     }
 }
 
-var time = 2000;
+var time = 3000;
 var slideStart = setInterval("count()",time);
 
 function slideView(n){
@@ -28,11 +30,7 @@ function slideView(n){
     $("#slideImg2 img").eq(n).stop().addClass("topImg").css("opacity",0).animate({"opacity":"1"},time-500);
 }
 
-$("#container").hover(function(){
-    clearInterval(slideStart);
-},function(){
-    slideStart = setInterval("count()",time);}
-);
+
 
 
 /*게시판*/
