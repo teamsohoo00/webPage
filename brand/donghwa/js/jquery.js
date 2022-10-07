d0fe8154f8cc23cb135c8a5faf2f1349cdd7f3b7
한지훈 <uuiop0129@gmail.com>
2022-10-07 17:23:58 +0900

    10.07 마무리,...

--- brand/donghwa/js/jquery.js
+++ brand/donghwa/js/jquery.js
@@ -17,7 +17,7 @@ function count(){
     }
 }
 
-var time = 2000;
+var time = 3000;
 var slideStart = setInterval("count()",time);
 
 function slideView(n){
@@ -28,11 +28,6 @@ function slideView(n){
     $("#slideImg2 img").eq(n).stop().addClass("topImg").css("opacity",0).animate({"opacity":"1"},time-500);
 }
 
-$("#container").hover(function(){
-    clearInterval(slideStart);
-},function(){
-    slideStart = setInterval("count()",time);}
-);
 
 
 /*게시판*/
